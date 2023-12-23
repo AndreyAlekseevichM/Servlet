426a4a62aabe29e2a9b704490602ec7eba78099b
Andrey <89024406064@mail.ru>
2023-12-24 02:51:03 +0500

    DI

--- src/main/java/ru/netology/servlet/Servlet.java
+++ src/main/java/ru/netology/servlet/Servlet.java
@@ -1,7 +1,9 @@
 package ru.netology.servlet;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.repository.PostRepositoryStubImpl;
+import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,8 +18,9 @@ public class Servlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var context = new AnnotationConfigApplicationContext("ru.netology");
-        PostController postController = context.getBean(PostController.class);
+        final var repository = new PostRepository();
+        final var service = new PostService((PostRepositoryStubImpl) repository);
+        controller = new PostController(service);
 
     }
 
